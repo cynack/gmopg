959c4f5fd984a122f48bf8598568157dd525a9cc
Kohei Futatsuka <futahei1108@icloud.com>
2022-12-26 06:57:56 +0000

    add post form data

--- src/client.ts
+++ src/client.ts
@@ -37,6 +37,26 @@ export default class Client {
     return parsed as U
   }
 
+  public async postXwwwFormUrlencoded<T extends { [key: string]: any }, U>(pathname: string, data: T): Promise<U> {
+    const params = new URLSearchParams()
+    Object.keys(data).forEach(key => {
+      params.append(key, data[key])
+    })
+    const res: Response = await fetch(this.config.baseUrl + pathname, {
+      method: 'POST',
+      body: params,
+      ...this.config.http,
+    })
+
+    const parsed: any = JSON.parse(await res.text())
+
+    if (!res.ok || this.isError(parsed)) {
+      throw new BadRequest(`Bad Request: ${pathname}`).setResponse(res).parseError(parsed)
+    }
+
+    return parsed as U
+  }
+
   public isError(res: UnknownParams): boolean {
     return !(res !== undefined && res.ErrCode === undefined)
   }
