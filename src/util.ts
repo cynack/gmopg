d9e08ce520033a8584c24ad51d7dc10de8e86ab2
Kohei Futatsuka <futahei1108@icloud.com>
2022-12-26 06:57:51 +0000

    add utils

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,18 @@
-import { createHash } from 'crypto'
+import { RSA_PKCS1_PADDING } from 'constants'
+import { createHash, publicEncrypt } from 'crypto'
 
 export function generateID(key: string): string {
   return `${key}-${createHash('md5').update(`${key}-${new Date().toISOString()}`).digest('hex')}`
+}
+
+export function createEncrypted(
+  key: string,
+  card: { cardNo: string; expire: string; securityCode?: string; holderName?: string; tokenNumber?: number }
+): string {
+  return publicEncrypt(
+    { key: `-----BEGIN PUBLIC KEY-----\n${key}\n-----END PUBLIC KEY-----`, padding: RSA_PKCS1_PADDING },
+    Buffer.from(JSON.stringify(card))
+  ).toString('base64')
 }
 
 export type Constructor<T> = new (...args: any[]) => T
